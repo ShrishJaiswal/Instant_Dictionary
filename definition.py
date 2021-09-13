f3b7d65d7362abbdcccf1fd39155a479a55df2ac
ShrishJaiswal <jaiswal.shrish@gmail.com>
2021-09-13 18:44:20 +0530

    Added dictionary dataset and changes to class file

--- definition.py
+++ definition.py
@@ -1,6 +1,13 @@
+import pandas as pd
+
+
+
 class Definition:
     def __init__(self, term):
         self.term=term
     def get(self):
-        pass
-    
+        df=pd.read_csv('dictionary.csv')
+        return tuple(df.loc[df['word']==self.term]['definition'])
+
+d=Definition(term='Farmer')
+print(d.get())
